0d47e72290833ae334ee99a2ebd60a71c29bfbd5
ydbb <yeondububub@gmail.com>
2026-06-19 17:24:15 +0900

    feat: 대기열 스케쥴러 추가

--- webflux/src/main/java/com/example/webflux/service/QueueService.java
+++ webflux/src/main/java/com/example/webflux/service/QueueService.java
@@ -3,17 +3,28 @@ package com.example.webflux.service;
 import com.example.webflux.exception.QueueErrorCode;
 import com.example.webflux.repository.RedisRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.zset.Tuple;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
 import java.util.Objects;
 
+import static reactor.netty.http.HttpConnectionLiveness.log;
+
 @Service
 @RequiredArgsConstructor
 public class QueueService {
 
     private final RedisRepository redisRepository;
+
+    @Value("${scheduler.enabled}")
+    private boolean schedulerEnabled = false;
+
+    @Value("${scheduler.max-allow-user-count}")
+    private long maxAllowUserCount = 0;
 
     public Mono<Long> enqueueWaitingQueue(Long userId) {
         long unixTimestamp = Instant.now().getEpochSecond();
@@ -36,6 +47,17 @@ public class QueueService {
                 .count();
     }
 
+    public Mono<Long> allow(String queue, Long count) {
+        return redisRepository.popMin(queue, count)
+                .flatMap(
+                        member -> redisRepository.addZSetIfAbsent(
+                                QueueManager.PROCEED_QUEUE.getKey(),
+                                Long.parseLong(Objects.requireNonNull(member.getValue())),
+                                Instant.now().getEpochSecond())
+                )
+                .count();
+    }
+
     public Mono<Boolean> isAllowed(Long userId) {
         return redisRepository.zRank(QueueManager.PROCEED_QUEUE.getKey(), userId)
                 .defaultIfEmpty(-1L)
@@ -50,5 +72,21 @@ public class QueueService {
                         .onErrorResume(ex -> redisRepository.zRank(QueueManager.WAITING_QUEUE.getKey(),  userId)
                                 .map(i -> i >= 0 ? i + 1 : i))
                 );
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void allowWaitQueueUser() {
+        if (!schedulerEnabled) {
+            log.info("passed scheduling");
+            return;
+        }
+
+        log.info("process scheduling");
+        redisRepository.scan("wait:*", 100L)
+                .flatMap(queue -> allow(queue, maxAllowUserCount)
+                        .map(allowedCount -> Tuple.of(queue.getBytes(), allowedCount.doubleValue())))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue"
+                        .formatted(maxAllowUserCount, tuple.getScore().longValue(), new String(tuple.getValue()))))
+                .subscribe();
     }
 }
